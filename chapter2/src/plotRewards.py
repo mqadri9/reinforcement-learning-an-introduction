c148da42265f43ce5312f96383efb9bc57e5b847
mqadri93 <mohamadqadri93@gmail.com>
2019-06-08 00:25:19 -0400

    Add files via upload

---
+++ chapter2/src/plotRewards.py
@@ -0,0 +1,5 @@
+import matplotlib.pyplot as plt
+def plot_rewards(rewards):
+    plt.plot(rewards, label='test')
+    plt.savefig('/cygdrive/c/Users/moham/eclipse-workspace/test/images/figure_2_2.png')
+    
